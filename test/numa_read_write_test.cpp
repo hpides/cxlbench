57009631508f83c62ae54f7bfa2dba85ceb56250
Tobias Jordan <tobias.jordan@student.hpi.de>
2023-06-16 14:55:42 +0200

    Implement Tests Checking if Data is Allocated on the Correct NUMA Nodes (#105)
    Adds some Asserts to the `NumaReadWriteTests`, checking if the memory is
    allocated on the desired numa node.
    Implements #100.

--- test/numa_read_write_test.cpp
+++ test/numa_read_write_test.cpp
@@ -15,6 +15,10 @@
 #include "test_utils.hpp"
 #include "utils.hpp"
 
+#ifdef HAS_NUMA
+#include <numaif.h>
+#endif
+
 namespace {
 constexpr uint32_t MIB_IN_BYTES = 1024 * 1024;
 }
@@ -23,7 +27,22 @@ namespace mema {
 
 class NumaReadWriteTest : public BaseTest {
  protected:
-  void SetUp() override { init_numa({}); }
+  void SetUp() override {
+    init_numa({});
+  }
+
+  NumaNodeID get_numa_node_index_by_address(char* addr) {
+    auto node = int32_t{};
+
+    auto addr_ptr = reinterpret_cast<void*>(addr);
+    auto ret = move_pages(0, 1, &addr_ptr, NULL, &node, 0);
+
+    if (ret != 0) {
+      throw std::runtime_error("move_pages() failed to determine NUMA node for address.");
+    }
+
+    return static_cast<NumaNodeID>(node);
+  }
 };
 
 TEST_F(NumaReadWriteTest, SimpleWriteRead) {
@@ -47,7 +66,10 @@ TEST_F(NumaReadWriteTest, SimpleWriteRead) {
       ASSERT_FALSE((addresses.contains(addr)));
       addresses.insert(addr);
       ASSERT_TRUE((addresses.contains(addr)));
+      // Check if data is allocated on the correct numa node.
+      ASSERT_EQ(get_numa_node_index_by_address(addr), numa_idx);
     }
+
     munmap(base_addr, memory_region_size);
   }
 }
@@ -76,7 +98,10 @@ TEST_F(NumaReadWriteTest, AVX512Read) {
       const auto compare_result = std::memcmp(read_cache_line, rw_ops::WRITE_DATA, rw_ops::CACHE_LINE_SIZE);
       // check if all compared bytes were equal.
       ASSERT_EQ(compare_result, 0);
+      // Check if data is allocated on the correct numa node.
+      ASSERT_EQ(get_numa_node_index_by_address(addr), numa_idx);
     }
+
     munmap(base_addr, memory_region_size);
   }
 }
@@ -105,7 +130,10 @@ TEST_F(NumaReadWriteTest, AVX512WriteRead) {
       const auto compare_result = std::memcmp(read_cache_line, rw_ops::WRITE_DATA, rw_ops::CACHE_LINE_SIZE);
       // check if all compared bytes were equal.
       ASSERT_EQ(compare_result, 0);
+      // Check if data is allocated on the correct numa node.
+      ASSERT_EQ(get_numa_node_index_by_address(addr), numa_idx);
     }
+
     munmap(base_addr, memory_region_size);
   }
 }
