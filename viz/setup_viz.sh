e93c6e60dab720b144f88fa3252cd2fd3989b57a
mweisgut <marcel.weisgut@hpi.de>
2023-09-26 09:51:38 +0200

    Fix viz setup

--- viz/setup_viz.sh
+++ viz/setup_viz.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
 if [ ! -d "$DIR/venv" ]; then
-    set -e
     virtualenv "$DIR/venv"
     source "$DIR/venv/bin/activate"
     pip3 install -r "$DIR/requirements.txt"
