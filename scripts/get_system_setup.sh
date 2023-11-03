c8167f1e688940a040311f2ca4f00b8ae3496aab
Marcel Weisgut <marcel.weisgut@lx01.cluster.delab.i.hpi.de>
2023-11-03 13:45:43 +0100

    Script: Fix output: get_system_setup.sh

--- scripts/get_system_setup.sh
+++ scripts/get_system_setup.sh
@@ -2,17 +2,17 @@
 
 # This script might not work on all machines. Feel free to open an issue mentioning the system and error message that
 # you receive when executing the script.
-echo -e "\n### Performance govenor\n" &&
+echo -e "\n### Perfoemance govenor\n"
 cpupower frequency-info
 
-echo -e "\n### Frequency boosting\n" &&
+echo -e "\n### Frequency boosting\n"
 echo cat /sys/devices/system/cpu/cpufreq/boost:
-cat /sys/devices/system/cpu/cpufreq/boost &&
+cat /sys/devices/system/cpu/cpufreq/boost
 
-echo -e "\n### SMT\n" &&
+echo -e "\n### SMT\n"
 echo cat /sys/devices/system/cpu/smt/control:
-cat /sys/devices/system/cpu/smt/control &&
+cat /sys/devices/system/cpu/smt/control
 
-echo -e "\n### Allocated huge pages\n" &&
+echo -e "\n### Allocated huge pages\n"
 echo cat /proc/sys/vm/nr_hugepages:
 cat /proc/sys/vm/nr_hugepages
