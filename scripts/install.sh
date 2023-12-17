1b93bbecbe010b109d921084db2e5668a47753d4
Marcel Weisgut <marcel.weisgut@hpi.de>
2023-12-17 13:48:29 +0100

    Add install script (#277)

---
+++ scripts/install.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+sudo apt update &&
+sudo apt install -y \
+    cmake \
+    dmidecode \
+    g++-12 \
+    gcc-12 \
+    gdb \
+    git \
+    hwloc \
+    libnuma-dev \
+    lshw \
+    ninja-build \
+    numactl \
+    pciutils \
+    python3-pip \
+    ruby \
+    vim \
+    zlib1g-dev
