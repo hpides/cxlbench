36d21aa1b8eedff0982e8664c7ed706ab308d69d
mweisgut <marcel.weisgut@hpi.de>
2024-05-03 17:30:38 +0200

    Documentation

--- src/read_write_ops_types.hpp
+++ src/read_write_ops_types.hpp
@@ -16,16 +16,17 @@ static constexpr auto CACHE_LINE_SIZE = size_t{64};
 
 // We use platform-independent vector intrinsics (compiler intrinsics) to avoid writing platform-specific SIMD
 // intrinsics (only for char data type; see definition of CharVec). VECTOR_SIZE defines the vector size and, in the case
-// of type char, the number of Byte being accessed. We assume 64 Byte as a cache line, which is the base access size in
-// this benchmark tool. We need a certain number of vectorized memory accesses to access 64 B. This number is determined
-// by CACHE_LINE_SIZE, which is 64 B / VECTOR_SIZE.
+// of type char, the number of Bytes being accessed. 64 B (cache line size) is the base access size in this benchmark
+// tool. We need a certain number of vectorized memory accesses to access 64 B. This number is determined by
+// CACHE_LINE_FACTOR.
+
 #ifdef USE_AVX_2
 static constexpr size_t VECTOR_SIZE = 32;
-static constexpr size_t CACHE_LINE_FACTOR = 2;
 #else
 static constexpr size_t VECTOR_SIZE = 64;
-static constexpr size_t CACHE_LINE_FACTOR = 1;
 #endif
+
+static constexpr size_t CACHE_LINE_FACTOR = CACHE_LINE_SIZE / VECTOR_SIZE;
 
 using CharVec __attribute__((vector_size(VECTOR_SIZE))) = char;
 
@@ -45,12 +46,13 @@ inline void no_barrier() {}
 
 template <typename Fn, size_t... indices>
 void unroll_impl(Fn fn, std::index_sequence<indices...>) {
-  // Call fn for all indices
-  (void(fn(indices)), ...);
+  // Call fn for sequence of passed indices.
+  (fn(indices), ...);
 }
 
 template <int LOOP_COUNT, typename Fn>
 void unroll(Fn fn) {
+  // Call unroll_impl with passed function and a compile-time sequence of integers [0, ..., LOOP_COUNT].
   unroll_impl(fn, std::make_index_sequence<LOOP_COUNT>());
 }
 
